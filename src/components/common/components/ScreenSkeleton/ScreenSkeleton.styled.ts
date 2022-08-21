87ae4802fd48f366c739526e794375ac883e2898
andrewvitrenko <avitrenko2004@gmail.com>
2022-08-21 22:28:01 +0200

    feat: added screen skeleton

---
+++ src/components/common/components/ScreenSkeleton/ScreenSkeleton.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Box } from '@mui/material';
+
+export const Wrapper = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2.5rem 2rem;
+`;
+
+export const Content = styled(Box)`
+  flex-grow: 1;
+  max-width: 1400px;
+`;
