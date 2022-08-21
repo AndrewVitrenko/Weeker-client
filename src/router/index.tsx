65b5b3ccbaf1fb459cd572e29144d65f37c6c5e9
andrewvitrenko <avitrenko2004@gmail.com>
2022-08-21 22:30:31 +0200

    feat(router): replaced inline values with enum

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,23 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ScreenSkeleton from '../components/common/components/ScreenSkeleton';
 import Header from '../components/Header';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
+import { ROUTES } from '../constants';
 
 export const Router: FC = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <ScreenSkeleton>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Home />} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
+          <Route path={ROUTES.SIGNUP} element={<Signup />} />
+        </Routes>
+      </ScreenSkeleton>
     </BrowserRouter>
   );
 };
